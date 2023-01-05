9623bb442dc578cb89c14003779c4d52b050a027
Shaheen Hyder <shaheenhyderk@gmail.com>
2023-01-05 17:18:54 +0530

    some stuctural change and made js file like a object based aproach

--- slideon.js
+++ slideon.js
@@ -2,17 +2,20 @@
  * Js for Slideon 1.0.0
  */
 
-; (function () {
-    var elements = document.querySelectorAll('.slideon.slideon-auto');
-    elements.forEach(function (element) {
-        var wrapper = document.createElement('label')
-        wrapper.className = element.classList
+function Slideon() {
+    this.load = function () {
+        var elements = document.querySelectorAll('.slideon.slideon-auto');
+        elements.forEach(function (element) {
+            element.classList.remove("slideon-auto")
+            var wrapper = document.createElement('label')
+            wrapper.className = element.classList
 
-        var slider = document.createElement('span')
-        slider.className = 'slideon-slider'
+            var slider = document.createElement('span')
+            slider.className = 'slideon-slider'
 
-        element.after(wrapper)
-        wrapper.appendChild(element)
-        element.after(slider)
-    });
-}());
+            element.after(wrapper)
+            wrapper.appendChild(element)
+            element.after(slider)
+        });
+    }
+}
